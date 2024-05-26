df97bbfac1d6e8099f4b0806961275f26e5ec5f5
BNikfal05 <85412678+BNikfal05@users.noreply.github.com>
2024-05-26 12:35:37 -0400

    Add files via upload
    This is a simple trivia game utilizing the database and API from https://opentdb.com/.
    For simplicity, when changing the questions you should copy paste this code's available database then go to ChatGPT and copy paste it there along with the new database from open trivia database. Then, ask it to format it in a similar fashion and compare what you got.
    
    Code was inspired from a Python Bootcamp course.

---
+++ Question_Trivia_Game/data.py
@@ -0,0 +1,83 @@
+question_data = [
+
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Books",
+        "question": "Stephen Chbosky wrote the book 'The Perks of Being A Wallflower'.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Books",
+        "question": "\"Green Eggs and Ham\" consists of only 50 different words.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Entertainment: Books",
+        "question": "In Harry Potter and the Deathly Hallows, Hermione destroyed the fourth Horcrux by stabbing it with the Sword of Gryffindor.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Books",
+        "question": "The \"Berenstein Bears\" is the correct spelling of the educational children's book series' name.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Books",
+        "question": "\"Elementary, my dear Watson\" is a phrase that is never truly said within the Conan Doyle books of Sherlock Holmes.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Books",
+        "question": "The book 1984 was published in 1949.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "hard",
+        "category": "Entertainment: Books",
+        "question": "Harry Potter was born on July 31st, 1980.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Books",
+        "question": "The Harry Potter series of books, combined, are over 1,000,000 words in length.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "medium",
+        "category": "Entertainment: Books",
+        "question": "Originally, the character Charlie from Charlie and the Chocolate Factory was going to be black.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "type": "boolean",
+        "difficulty": "easy",
+        "category": "Entertainment: Books",
+        "question": "Shub-Niggurath is a creature that was created by J. R. R. Tolkien in his novel \"The Lord of The Rings\".",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }
+]
